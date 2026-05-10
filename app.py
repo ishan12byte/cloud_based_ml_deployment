8bb7ffd15cb87352dacb7412df814e31cb23b2de
ishan12byte <ishangupta.cpu@gmail.com>
2026-05-10 15:37:10 +0000

    "Added flask app and models with html webpage

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import pickle
+
+tokenizer=pickle.load(open("models/cv.pkl", "rb"))
+model=pickle.load(open("models/clf.pkl","rb"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email_text = request.form.get("email-content")
+    tokenized_email=tokenizer.transform([email_text])
+    predictions = model.predict(tokenized_email)
+    predictions = 1 if predictions == 1 else -1
+    return render_template("index.html", predictions=predictions, email=email_text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
